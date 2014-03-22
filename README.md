a030fc33e4d33a81f1777be3ab1e4c9be7c82379
Adam Couper <lokifer@gmail.com>
2014-03-23 11:21:39 +1300

    Updated README, Vagrantfile, install.sh
    Changed install.sh to my version. Moved vagrantfile over to 64, updated
    readme to reflect this

--- README.md
+++ README.md
@@ -1,21 +1,14 @@
 vagrant-django-base
 ===================
 
-A Vagrant box based on Ubuntu precise32, configured for Django development
-according to Torchbox's adopted practices. Things preinstalled beyond the base
-precise32 box include:
+A Vagrant box based on Ubuntu precise64, configured for Django 1.6 development.
 
-* postgresql 9.1 (with locale fixed to create databases as UTF-8)
+* postgresql 9.3 (with locale fixed to create databases as UTF-8)
 * virtualenv and virtualenvwrapper
 * dependencies for PIL, the Python Imaging Library
 * a pip download cache pre-seeded with Django and various other common packages
 * git (sometimes required for pip dependencies that aren't in PyPI)
 * Node.js, CoffeeScript and LESS
-
-We use this box in conjunction with https://github.com/torchbox/vagrant-django-template
-as the initial template for our Django projects. vagrant-django-template will
-successfully build from a vanilla precise32 base box, but using vagrant-django-base
-instead will skip some of the time-consuming initial setup.
 
 Build instructions
 ------------------
--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 # Script to set up dependencies for Django on Vagrant.
 
-PGSQL_VERSION=9.1
+PGSQL_VERSION=9.3
 
 # Need to fix locale so that Postgres creates databases in UTF-8
 cp -p /vagrant_data/etc-bash.bashrc /etc/bash.bashrc
@@ -16,7 +16,7 @@ export LC_ALL=en_GB.UTF-8
 # Install essential packages from Apt
 apt-get update -y
 # Python dev packages
-apt-get install -y build-essential python python-dev python-setuptools python-pip
+apt-get install -y build-essential python python-dev python-setuptools python-pip ruby1.9.3
 # Dependencies for image processing with PIL
 apt-get install -y libjpeg-dev libtiff-dev zlib1g-dev libfreetype6-dev liblcms2-dev
 # Git (we'd rather avoid people keeping credentials for git commits in the repo, but sometimes we need it for pip requirements that aren't in PyPI)
@@ -28,13 +28,11 @@ if ! command -v psql; then
     cp /vagrant_data/pg_hba.conf /etc/postgresql/$PGSQL_VERSION/main/
     /etc/init.d/postgresql reload
 fi
-
+# Install virtualenv etc
+apt-get install -y virtualenvwrapper
 # virtualenv global setup
 if ! command -v pip; then
     easy_install -U pip
-fi
-if [[ ! -f /usr/local/bin/virtualenv ]]; then
-    easy_install virtualenv virtualenvwrapper stevedore virtualenv-clone
 fi
 
 # bash environment global setup
@@ -48,18 +46,91 @@ if [[ ! -e /home/vagrant/.pip_download_cache ]]; then
         rm -rf /home/vagrant/yayforcaching"
 fi
 
-# Node.js, CoffeeScript and LESS
-if ! command -v npm; then
-    wget http://nodejs.org/dist/v0.10.0/node-v0.10.0.tar.gz
-    tar xzf node-v0.10.0.tar.gz
-    cd node-v0.10.0/
-    ./configure && make && make install
-    cd ..
-    rm -rf node-v0.10.0/ node-v0.10.0.tar.gz
+# Edit the following to change the name of the database user that will be created:
+APP_DB_USER=vagrant
+APP_DB_PASS=sreegsahuj9
+
+# Edit the following to change the name of the database that is created (defaults to the user name)
+APP_DB_NAME=veganarium
+
+# Edit the following to change the version of PostgreSQL that is installed
+PG_VERSION=9.3
+
+###########################################################
+# Changes below this line are probably not necessary
+###########################################################
+print_db_usage () {
+  echo "Your PostgreSQL database has been setup and can be accessed on your local machine on the forwarded port (default: 15432)"
+  echo "  Host: localhost"
+  echo "  Port: 15432"
+  echo "  Database: $APP_DB_NAME"
+  echo "  Username: $APP_DB_USER"
+  echo "  Password: $APP_DB_PASS"
+  echo ""
+  echo "Admin access to postgres user via VM:"
+  echo "  vagrant ssh"
+  echo "  sudo su - postgres"
+  echo ""
+  echo "psql access to app database user via VM:"
+  echo "  vagrant ssh"
+  echo "  sudo su - postgres"
+  echo "  PGUSER=$APP_DB_USER PGPASSWORD=$APP_DB_PASS psql -h localhost $APP_DB_NAME"
+  echo ""
+  echo "Env variable for application development:"
+  echo "  DATABASE_URL=postgresql://$APP_DB_USER:$APP_DB_PASS@localhost:15432/$APP_DB_NAME"
+  echo ""
+  echo "Local command to access the database via psql:"
+  echo "  PGUSER=$APP_DB_USER PGPASSWORD=$APP_DB_PASS psql -h localhost -p 15432 $APP_DB_NAME"
+}
+
+export DEBIAN_FRONTEND=noninteractive
+
+PROVISIONED_ON=/etc/vm_provision_on_timestamp
+if [ -f "$PROVISIONED_ON" ]
+then
+  echo "VM was already provisioned at: $(cat $PROVISIONED_ON)"
+  echo "To run system updates manually login via 'vagrant ssh' and run 'apt-get update && apt-get upgrade'"
+  echo ""
+  print_db_usage
+  exit
 fi
-if ! command -v coffee; then
-    npm install -g coffee-script
+
+PG_REPO_APT_SOURCE=/etc/apt/sources.list.d/pgdg.list
+if [ ! -f "$PG_REPO_APT_SOURCE" ]
+then
+  # Add PG apt repo:
+  echo "deb http://apt.postgresql.org/pub/repos/apt/ precise-pgdg main" > "$PG_REPO_APT_SOURCE"
+
+  # Add PGDG repo key:
+  wget --quiet -O - http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | apt-key add -
 fi
-if ! command -v lessc; then
-    npm install -g less
-fi
+
+apt-get -y install "postgresql-$PG_VERSION" "postgresql-contrib-$PG_VERSION"
+
+PG_CONF="/etc/postgresql/$PG_VERSION/main/postgresql.conf"
+PG_HBA="/etc/postgresql/$PG_VERSION/main/pg_hba.conf"
+PG_DIR="/var/lib/postgresql/$PG_VERSION/main"
+
+# Edit postgresql.conf to change listen address to '*':
+sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/" "$PG_CONF"
+
+# Append to pg_hba.conf to add password auth:
+echo "host    all             all             all                     md5" >> "$PG_HBA"
+
+# Restart so that all new config is loaded:
+service postgresql restart
+
+cat << EOF | su - postgres -c psql
+-- Create the database user:
+CREATE USER $APP_DB_USER WITH PASSWORD '$APP_DB_PASS';
+
+-- Create the database:
+CREATE DATABASE $APP_DB_NAME WITH OWNER $APP_DB_USER;
+EOF
+
+# Tag the provision time:
+date > "$PROVISIONED_ON"
+
+echo "Successfully created PostgreSQL dev virtual machine."
+echo ""
+print_db_usage
